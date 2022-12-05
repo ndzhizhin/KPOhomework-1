96967ac1118a29adb75809aaedc18829471d4eb8
ndzhizhin <99075109+ndzhizhin@users.noreply.github.com>
2022-12-05 22:43:42 +0300

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    private Integer score = 2;
+    public Integer getScore() {
+        return score;
+    }
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
